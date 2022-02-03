901a70681a210e3413c468e768e7dd96c9e6f336
Julian Falquez Ruiz <julian.falquezr@perficient.com>
2022-02-03 16:23:19 -0500

    Cambio a Header, se removio div

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,17 @@ import React from "react";
 
 import CartButton from "../Cart/CartButton";
 import "./Header.css";
-import Logo from './Logo';
+import react_logo from "../../UI/react_logo.png";
 
 export default function Header() {
   return (
-    <header>
-      <div className="header_container">
-          <Logo />
-          <CartButton />
+    <header className="header_container">
+      <div>
+        <img className="logo" src={react_logo} alt="react_logo" />
+        <h1 className="logo_text">SPT</h1>
+      </div>
+      <div>
+        <CartButton />
       </div>
     </header>
   );
